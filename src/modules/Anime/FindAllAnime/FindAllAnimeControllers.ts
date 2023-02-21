ae78dcfd3ce94a8df47e4da2210c8099904d0b8a
Joestar117 <juniorrios1231@gmail.com>
2023-02-21 13:39:31 -0300

    feat: router findAll animes created✔

---
+++ src/modules/Anime/FindAllAnime/FindAllAnimeControllers.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { database } from "../../../database/database";
+
+export class FindAllAnimeController {
+  async handle(req: Request, res: Response) {
+    try {
+      const animes = await database.anime.findMany({
+        include: {
+          AnimeRent: true,
+        },
+      });
+      return res.status(200).json(animes);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
--- src/routes/Anime.Routes.ts
+++ src/routes/Anime.Routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateAnimeControllers } from "../modules/Anime/CreateAnime/CreateAnimeControllers";
+import { FindAllAnimeController } from "../modules/Anime/FindAllAnime/FindAllAnimeControllers";
 
-const createAnimeControllers = new CreateAnimeControllers()
+const createAnimeControllers = new CreateAnimeControllers();
+const findAllAnimeControllers = new FindAllAnimeController();
 
-const anime = Router()
+const anime = Router();
 
-anime.post("/anime",createAnimeControllers.handle)
+anime.post("/anime", createAnimeControllers.handle);
+anime.get("/anime", findAllAnimeControllers.handle);
 
-export {anime}
+export { anime };

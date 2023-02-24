ebe2edf26213ece6ddb54c60ef281c8c15e0c73f
Joestar117 <juniorrios1231@gmail.com>
2023-02-24 15:19:46 -0300

    feat: Controllers refector ✔

--- src/modules/User/UpdateUser/UpdateUserUseCase.ts
+++ src/modules/User/UpdateUser/UpdateUserUseCase.ts
@@ -5,15 +5,6 @@ import bcrypt from "bcrypt";
 
 export class UpdateUserUseCase {
   async execute({ id, email, name, password }: UserDTO) {
-    const idIsValid = await database.user.findUnique({
-      where: {
-        id: id,
-      },
-    });
-    if (!idIsValid) {
-      return new AppError("id is not valid");
-    }
-
     const hash = await bcrypt.hash(password, 8);
 
     const user = await database.user.update({

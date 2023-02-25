a669d351af25b76a6d8f80d7135e2d947baf32d9
Joestar117 <juniorrios1231@gmail.com>
2023-02-25 17:08:27 -0300

    feat: Admin refactor controller

--- src/modules/Admin/LoginAdmin/LoginAdminController.ts
+++ src/modules/Admin/LoginAdmin/LoginAdminController.ts
@@ -1,43 +1,17 @@
 import { Request, Response } from "express";
-import { database } from "../../../database/database";
-import { AdminDTO } from "../AdminDTO";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { LoginAdminUseCase } from "./LoginAdminUseCase";
 
 export class LoginAdminController {
   async handle(req: Request, res: Response) {
-    const { email, password, id }: AdminDTO = req.body;
-
-    const UserAdmin: string = process.env.ADMIN as string;
-
+    const { email, password } = req.body;
     try {
-      const userIsValid = await database.admin.findFirst({
-        where: {
-          email: email,
-        },
-      });
-
-      if (!userIsValid) {
-        return res.status(400).json({ message: "User or Password incorrect" });
-      }
-
-      const passwordIsValid = await bcrypt.compare(
+      const loginAdminUseCase = new LoginAdminUseCase();
+      const result = await loginAdminUseCase.execute({
+        email,
         password,
-        userIsValid.password
-      );
-
-      if (!passwordIsValid) {
-        return res.status(400).json({ message: "User or Password incorrect" });
-      }
-
-      const tokenAdmin = jwt.sign({ id: id }, UserAdmin, {
-        expiresIn: 86400,
       });
 
-      res.json(tokenAdmin);
+      return res.status(200).json(result);
     } catch (error) {
       console.log(error);
     }
---
+++ src/modules/Admin/LoginAdmin/LoginAdminUseCase.ts
@@ -0,0 +1,42 @@
+import { AdminDTO } from "../AdminDTO";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { database } from "../../../database/database";
+import { AppError } from "../../../errors/AppError";
+
+dotenv.config();
+
+export class LoginAdminUseCase {
+  async execute({ email, password, id }: AdminDTO) {
+    const UserAdmin: string = process.env.ADMIN as string;
+
+    try {
+      const userIsValid = await database.admin.findFirst({
+        where: {
+          email: email,
+        },
+      });
+
+      if (!userIsValid) {
+        return new AppError("User or Password incorrect");
+      }
+
+      const passwordIsValid = await bcrypt.compare(
+        password,
+        userIsValid.password
+      );
+
+      if (!passwordIsValid) {
+        return new AppError("User or Password incorrect");
+      }
+      const tokenAdmin = jwt.sign({ id: id }, UserAdmin, {
+        expiresIn: 86400,
+      });
+
+      return tokenAdmin;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}

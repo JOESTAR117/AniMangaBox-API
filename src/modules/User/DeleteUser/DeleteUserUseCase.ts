597c7f44c0979b2008199fed6ad1835d366f0f5e
Joestar117 <juniorrios1231@gmail.com>
2023-02-26 14:21:51 -0300

    feat: Delete user created 2/4✔

--- src/modules/User/DeleteUser/DeleteUserUseCase.ts
+++ src/modules/User/DeleteUser/DeleteUserUseCase.ts
@@ -1,8 +1,19 @@
 import { database } from "../../../database/database";
+import { AppError } from "../../../errors/AppError";
 import { DeleteUserDTO } from "./DeleteUserDTO";
 
 export class DeleteUserUseCase {
   async execute({ id }: DeleteUserDTO) {
+    const userRentedAnime = await database.animeRent.findMany({
+      where: {
+        userId: id,
+      },
+    });
+
+    if (userRentedAnime.length > 0) {
+      return new AppError("user has rented anime");
+    }
+
     const user = await database.user.delete({
       where: {
         id: id,

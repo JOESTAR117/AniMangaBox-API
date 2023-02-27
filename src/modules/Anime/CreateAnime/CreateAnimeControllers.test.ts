95f1de0ee24a28692c11a56923b4e176a797365d
Joestar117 <juniorrios1231@gmail.com>
2023-02-27 13:14:38 -0300

    feat: test createAnimePassed✔

---
+++ src/modules/Anime/CreateAnime/CreateAnimeControllers.test.ts
@@ -0,0 +1,24 @@
+import { Request } from "express";
+import { makeMockResponse } from "../../../../utils/mocks/makeMockResponse";
+import { CreateAnimeControllers } from "./CreateAnimeControllers";
+
+const createAnime = new CreateAnimeControllers();
+
+describe("CreateUserController", () => {
+  const res = makeMockResponse();
+
+  it("Should return the status 201 of the created anime", async () => {
+    const req = {
+      body: {
+        title: "teste",
+        description: "teste",
+        episodes: "30",
+        release_date: "2013-04-07T16:53:52.899Z",
+      },
+    } as Request;
+
+    await createAnime.handle(req, res);
+
+    expect(res.state.status).toEqual(201);
+  });
+});

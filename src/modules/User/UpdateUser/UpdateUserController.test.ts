e8ca3c88e69c0a515a3188cdfa13450775607417
Joestar117 <juniorrios1231@gmail.com>
2023-02-27 12:57:56 -0300

    feat: test updateUser passed ✔

---
+++ src/modules/User/UpdateUser/UpdateUserController.test.ts
@@ -0,0 +1,26 @@
+import { Request } from "express";
+import { makeMockResponse } from "../../../../utils/mocks/makeMockResponse";
+import { UpdateUserController } from "./UpdateUserController";
+
+const updateUser = new UpdateUserController();
+
+describe("UpdateUserController", () => {
+  const res = makeMockResponse();
+
+  it("should respond with 200 when user is updated", async () => {
+    const req = {
+      params: {
+        id: "234324234242",
+      },
+      body: {
+        name: "Bob",
+        email: "bob@gmail.com",
+        password: 123,
+      },
+    } as unknown as Request;
+
+    await updateUser.handle(req, res);
+
+    expect(res.state.status).toEqual(200);
+  });
+});

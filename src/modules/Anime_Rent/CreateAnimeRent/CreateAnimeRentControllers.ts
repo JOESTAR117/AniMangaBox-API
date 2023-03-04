d229503bdee6171f99ccd02259861522bf6ef3bd
Joestar117 <juniorrios1231@gmail.com>
2023-03-04 14:50:38 -0300

    FIX: solving the bug of renting anime

--- src/modules/Anime_Rent/CreateAnimeRent/CreateAnimeRentControllers.ts
+++ src/modules/Anime_Rent/CreateAnimeRent/CreateAnimeRentControllers.ts
@@ -4,19 +4,14 @@ import { CreateAnimeRentUseCase } from './CreateAnimeRentUseCase'
 export class CreateAnimeRentControllers {
 	async handle(req: Request, res: Response) {
 		const { animeId, userId } = req.body
-		try {
-			const createAnimeRentUseCase = new CreateAnimeRentUseCase()
 
-			const result = await createAnimeRentUseCase.execute({
-				animeId,
-				userId,
-			})
+		const createAnimeRentUseCase = new CreateAnimeRentUseCase({
+			animeId,
+			userId,
+		})
 
-			return res
-				.status(201)
-				.json({ message: 'you rented this anime, enjoy' })
-		} catch (error) {
-			console.error(error)
-		}
+		await createAnimeRentUseCase.execute(req, res)
+
+		return res.status(201).json({ message: 'anime successfully rented' })
 	}
 }
--- src/modules/Anime_Rent/CreateAnimeRent/CreateAnimeRentUseCase.ts
+++ src/modules/Anime_Rent/CreateAnimeRent/CreateAnimeRentUseCase.ts
@@ -1,47 +1,49 @@
 import { database } from '../../../database/database'
-import { AppError } from '../../../errors/AppError'
 import { AnimeRentDTO } from '../../../dtos/Anime/AnimeRentDTO'
+import { Request, Response } from 'express'
 
 export class CreateAnimeRentUseCase {
-	async execute({ animeId, userId }: AnimeRentDTO) {
+	constructor(private data: AnimeRentDTO) {}
+	async execute(req: Request, res: Response) {
 		try {
 			const animeExists = await database.anime.findUnique({
 				where: {
-					id: animeId,
+					id: this.data.animeId,
 				},
 			})
+
+			if (!animeExists) {
+				res.status(400).json({ message: 'Anime does not exists!' })
+			}
+
+			const animeAlredyRented = await database.animeRent.findFirst({
+				where: {
+					animeId: this.data.animeId,
+				},
+			})
+
+			if (animeAlredyRented) {
+				res.status(400).json({
+					message: 'this anime has already been rented by you',
+				})
+			}
 
 			const userExists = await database.user.findUnique({
 				where: {
-					id: userId,
+					id: this.data.userId,
 				},
 			})
 
-			if (!animeExists || !userExists) {
-				return new Error('Anime or user does not exist!')
+			if (!userExists) {
+				res.status(400).json({ message: 'User does not exists!' })
 			}
 
-			const animeWasRented = await database.animeRent.findUnique({
-				where: {
-					userId_animeId: {
-						animeId: animeId,
-						userId: userId,
-					},
-				},
-			})
-
-			if (animeWasRented) {
-				return new AppError('this anime has already been rented by you')
-			}
-
-			const anime_rent = await database.animeRent.create({
+			await database.animeRent.create({
 				data: {
-					animeId: animeId,
-					userId: userId,
+					animeId: this.data.animeId,
+					userId: this.data.userId,
 				},
 			})
-
-			return anime_rent
 		} catch (error) {
 			return console.log(error)
 		}
